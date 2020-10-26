cb940295fa492481dfded8782aafa2ce811bf0e7
jinzeze <jinzeze@medlinker.com>
2020-10-26 20:43:57 +0800

    feat: 封装taro-request

---
+++ services/.history/interceptor_20201026203259.ts
@@ -0,0 +1,97 @@
+import Taro, { Chain, request } from '@tarojs/taro';
+import codeMessage from './codeMessage';
+
+enum HTTP_STATUS {
+  SUCCESS = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  CLIENT_ERROR = 400,
+  AUTHENTICATE = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  SERVER_ERROR = 500,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504
+}
+
+let REQUEST_NUM = 0;
+
+export const interceptor = (chain: Chain) => {
+  increaseRequest();
+  const requestParams = chain.requestParams;
+  const errorData = { data: { errcode: 1, errmsg: '', data: null } }
+  return chain.proceed(requestParams).then((res: request.SuccessCallbackResult<{ errcode: number, errmsg: string, data: any }>) => {
+    console.log('data->', res)
+    reduceRequest();
+    if (res.statusCode === HTTP_STATUS.NOT_FOUND) {
+      errShowToast('404->资源找不到');
+    } else if (res.statusCode === HTTP_STATUS.BAD_GATEWAY) {
+      errShowToast('502->服务端问题');
+    } else if (res.statusCode === HTTP_STATUS.FORBIDDEN) {
+      errShowToast('403->Forbidden')
+    } else if (res.statusCode === HTTP_STATUS.AUTHENTICATE) {
+      errShowToast('401->Unauthorized')
+    } else if (res.statusCode === HTTP_STATUS.SUCCESS) {
+      if (res.data.errcode === 0) {
+        return res;
+      } else if (codeMessage[res.data.errcode]) {
+        errShowToast('errorcode->'codeMessage[res.data.errcode])
+      }
+    } else {
+      errShowToast('statusError->服务器错误，请稍后重试')
+    }
+    return errorData;
+  }).catch(() => {
+    reduceRequest();
+    errShowToast('request:fail->接口请求超时失败')
+    return errorData;
+  })
+}
+
+const errShowToast = (title: string) => {
+  isHideLoading();
+  Taro.showToast({ title, icon: 'none', mask: true, duration: 1500 });
+  setTimeout(() => {
+    isShowLoading();
+  }, 1500);
+}
+
+const isHideLoading = () => {
+  if (REQUEST_NUM > 0) {
+    console.log('toast之前隐藏loading');
+    Taro.hideLoading();
+  }
+}
+
+const isShowLoading = () => {
+  if (REQUEST_NUM > 0) {
+    Taro.showLoading({
+      title: 'loading...',
+      mask: true,
+    });
+  }
+}
+
+const increaseRequest = () => {
+
+  if (REQUEST_NUM === 0) {
+    console.log('开始loading');
+    Taro.showLoading({
+      title: 'loading...',
+      mask: true,
+    });
+  }
+  REQUEST_NUM += 1;
+  console.log('REQUEST_NUM->', REQUEST_NUM)
+}
+
+const reduceRequest = () => {
+
+  REQUEST_NUM -= 1;
+  console.log('REQUEST_NUM->', REQUEST_NUM)
+  if (REQUEST_NUM === 0) {
+    console.log('结束loading');
+    Taro.hideLoading();
+  }
+}
